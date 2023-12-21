f8ddc9367d6cc1fb79a7051e48b9251f5c8f9370
Matthew Stanger <stangerm2@gmail.com>
2023-12-21 13:38:21 -0800

    nn_3_chan_lstm_action_game_coords

--- bin/run_training.py
+++ bin/run_training.py
@@ -18,9 +18,10 @@ from red_env_constants import *
 class CustomFeatureExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space, features_dim=64):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
-
+        self.coordinate_memory = None
         self.action_memory = None
         self.game_state_memory = None
+        self.step_count = 0
 
         # Define CNN architecture for spatial inputs
         self.cnn = nn.Sequential(
@@ -39,6 +40,7 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         self.game_state_embedding = nn.Embedding(num_embeddings=117, embedding_dim=8)
 
         # LSTM layers for action and game state embeddings
+        self.coord_lstm = nn.LSTM(input_size=21, hidden_size=features_dim, batch_first=True)
         self.action_lstm = nn.LSTM(input_size=56, hidden_size=features_dim, batch_first=True)
         self.game_state_lstm = nn.LSTM(input_size=936, hidden_size=features_dim, batch_first=True)
 
@@ -52,6 +54,7 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
     def forward(self, observations):
         batch_size = observations["visited"].size(0)  # Get dynamic batch size from screen
         device = observations["screen"].device  # Assuming 'screen' is part of your observations
+        self._init_memory(device)
 
         combined_input = torch.cat([observations["screen"].unsqueeze(1),
                                     observations["visited"].unsqueeze(1),
@@ -61,33 +64,27 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         screen_features = self.cnn(combined_input)
 
         # Process 'coordinates' and pass through fully connected layer
-        coordinates_input = observations["coordinates"].view(batch_size, -1)
-        coordinates_features = self.coordinates_fc(coordinates_input)
+        coordinates_input = observations["coordinates"].view(batch_size, -1).unsqueeze(0).to(device)
+        #coordinates_features = self.coordinates_fc(coordinates_input)
 
         # Explicitly use batch_size for reshaping
         action_input = self.action_embedding(observations["action"].int()).view(batch_size, -1).unsqueeze(0).to(device)
         game_state_input = self.game_state_embedding(observations["game_state"].int()).view(batch_size, -1).unsqueeze(0).to(device)
 
-        # Initialize hidden states if None
-        if self.action_memory is None:
-            self.action_memory = (torch.zeros(1, 1, self.features_dim, device=device),
-                                  torch.zeros(1, 1, self.features_dim, device=device))
-
-        if self.game_state_memory is None:
-            self.game_state_memory = (torch.zeros(1, 1, self.features_dim, device=device),
-                                      torch.zeros(1, 1, self.features_dim, device=device))
-
         # Process through LSTMs
+        _, self.coordinate_memory = self.coord_lstm(coordinates_input, self.coordinate_memory)
         _, self.action_memory = self.action_lstm(action_input, self.action_memory)
         _, self.game_state_memory = self.game_state_lstm(game_state_input, self.game_state_memory)
 
-        self._detach_hidden_states()
+        self._detach_hidden_states(device)
 
         # Extract LSTM final states
+        coordinates_features = self.coordinate_memory[0].squeeze(0).squeeze(0)
         action_lstm_features = self.action_memory[0].squeeze(0).squeeze(0)
         game_state_lstm_features = self.game_state_memory[0].squeeze(0).squeeze(0)
         action_lstm_features = action_lstm_features.unsqueeze(0).repeat(batch_size, 1)
         game_state_lstm_features = game_state_lstm_features.unsqueeze(0).repeat(batch_size, 1)
+        coordinates_features = coordinates_features.unsqueeze(0).repeat(batch_size, 1)
 
         combined_features = torch.cat([
             screen_features,
@@ -99,7 +96,31 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         # Ensure the input size to fc_combined matches the concatenated features size
         return self.fc_layers(combined_features)
     
-    def _detach_hidden_states(self):
+    def _init_memory(self, device):
+            # Initialize hidden states if None
+        if self.coordinate_memory is not None:
+            return
+        
+        self.coordinate_memory = (torch.zeros(1, 1, self.features_dim, device=device),
+                                    torch.zeros(1, 1, self.features_dim, device=device))
+
+        self.action_memory = (torch.zeros(1, 1, self.features_dim, device=device),
+                                torch.zeros(1, 1, self.features_dim, device=device))
+
+        self.game_state_memory = (torch.zeros(1, 1, self.features_dim, device=device),
+                                    torch.zeros(1, 1, self.features_dim, device=device))
+
+    def _detach_hidden_states(self, device):
+        self.step_count += 1
+
+        if self.step_count == 2048:
+            self.action_memory = None
+            self.game_state_memory = None
+            self.coordinate_memory = None
+            self.step_count = 0
+            self._init_memory(device)
+            return
+        
         # Detach both hidden state and cell state
         hidden_state, cell_state = self.action_memory
         hidden_state = hidden_state.detach()
@@ -114,6 +135,13 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
 
         # Reconstruct the tuple
         self.game_state_memory = (hidden_state, cell_state)
+
+        hidden_state, cell_state = self.coordinate_memory
+        hidden_state = hidden_state.detach()
+        cell_state = cell_state.detach()
+
+        # Reconstruct the tuple
+        self.coordinate_memory = (hidden_state, cell_state)
 
 
 def make_env(thread_id, env_conf, seed=0):
@@ -147,7 +175,7 @@ if __name__ == '__main__':
         'explore_weight': 3  # 2.5
     }
 
-    num_cpu = 124  # Also sets the number of episodes per training iteration
+    num_cpu = 1  # Also sets the number of episodes per training iteration
 
     if 0 < num_cpu < 50:
         env_config['debug'] = True
