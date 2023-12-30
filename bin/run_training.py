a107d9c0ecc8e9310314fe839ec7525a26c817e0
Matthew Stanger <stangerm2@gmail.com>
2023-12-30 11:53:22 -0800

    TEST: baseline_cont_rewrite
    Begining of long tuning testing commits with custom NN using
    cont space & layered FCL's.

--- bin/run_training.py
+++ bin/run_training.py
@@ -34,33 +34,80 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         )
 
         # Game Embeddings
-        self.action_embedding = nn.Embedding(num_embeddings=7, embedding_dim=8)
-        self.game_state_embedding = nn.Embedding(num_embeddings=117, embedding_dim=8)
+        #self.action_embedding = nn.Embedding(num_embeddings=7, embedding_dim=8)
+        #self.game_state_embedding = nn.Embedding(num_embeddings=117, embedding_dim=8)
 
-        # Player Embeddings
-        self.pokemon_lineup_embedding = nn.Embedding(num_embeddings=256, embedding_dim=16)
-        self.pokemon_health_embedding = nn.Embedding(num_embeddings=704, embedding_dim=8)
-        self.pokemon_stats_embedding = nn.Embedding(num_embeddings=459, embedding_dim=8)
+        # Move Class
+        self.player_moves_embedding = nn.Embedding(num_embeddings=256, embedding_dim=8)
+        self.move_max_pool = nn.AdaptiveMaxPool2d(output_size=(1, 16))
 
-        # Battle Embeddings
-        self.battle_type_embedding = nn.Embedding(num_embeddings=256, embedding_dim=4)
-        self.enemies_left_embedding = nn.Embedding(num_embeddings=256, embedding_dim=4)
-        self.player_stats_embedding = nn.Embedding(num_embeddings=256, embedding_dim=6)
-        self.enemy_stats_embedding = nn.Embedding(num_embeddings=256, embedding_dim=6)
-        self.turn_info_embedding = nn.Embedding(num_embeddings=256, embedding_dim=6)
-        self.type_hint_embedding = nn.Embedding(num_embeddings=256, embedding_dim=4)
+        self.move_fc = nn.Sequential(
+            nn.Linear(6168, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, 32),
+            nn.ReLU(),
+        )
+
+        # Pokemon Class
+        self.player_max_pool = nn.AdaptiveMaxPool2d(output_size=(1, 32))
+        self.pokemon_fc = nn.Sequential(
+            nn.Linear(1938, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, features_dim),
+            nn.ReLU(),
+        )
+
+        # Player Class
+        self.player_fc = nn.Sequential(
+            nn.Linear(96, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, 32),
+            nn.ReLU(),
+        )
+
+        # Player Fighter Class
+        self.player_fighter_fc = nn.Sequential(
+            nn.Linear(305, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, features_dim),
+            nn.ReLU(),
+        )
+
+        # Battle Turn Class
+        self.battle_turn_fc = nn.Sequential(
+            nn.Linear(123, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, 32),
+            nn.ReLU(),
+        )
+
+        # Enemy Battle Class
+        self.enemy_battle_fc = nn.Sequential(
+            nn.Linear(324, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, 32),
+            nn.ReLU(),
+        )
 
         # Battle FCL's
         self.battle_fc = nn.Sequential(
-            nn.Linear(1686, features_dim),  # Adjust the output size as needed
+            nn.Linear(150, features_dim),  # Adjust the output size as needed
             nn.ReLU(),
             #nn.Linear(64, features_dim),
             #nn.ReLU()
         )
 
+        # Battle Turn FCL
+        self.battle_turn_fc = nn.Sequential(
+            nn.Linear(613, features_dim),
+            nn.ReLU()
+        )
+
         # Fully connected layers for output
         self.fc_layers = nn.Sequential(
-            nn.Linear(1904, features_dim),
+            nn.Linear(1036, 256),
+            nn.ReLU(),
+            nn.Linear(256, features_dim),
             nn.ReLU()
         )
 
@@ -80,42 +127,114 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         coordinates_features = self.coordinates_fc(coordinates_input)
 
         # Game Embeddings
-        action_features = self.action_embedding(observations["action"].int()).view(batch_size, -1)
-        game_state_features = self.game_state_embedding(observations["game_state"].int()).view(batch_size, -1)
+        #action_features = self.action_embedding(observations["action"].int()).view(batch_size, -1)
+        #game_state_features = self.game_state_embedding(observations["game_state"].int()).view(batch_size, -1)
+        action_features = observations["action"].int().view(batch_size, -1)
+        game_state_features = observations["game_state"].int().view(batch_size, -1)
 
-        # Player Embeddings
-        pokemon_lineup_input = self.pokemon_lineup_embedding(observations["pokemon_lineup"].view(-1).int()).view(batch_size, -1)
-        pokemon_health_input = self.pokemon_health_embedding(observations["pokemon_health"].view(-1).int()).view(batch_size, -1)
-        pokemon_stats_input = self.pokemon_stats_embedding(observations["pokemon_stats"].view(-1).int()).view(batch_size, -1)
+        # Move Class
+        #player_moves_input = self.player_moves_embedding(observations["player_moves"].to(torch.int)).view(batch_size, -1)
+        player_moves_input = observations["player_moves"].view(batch_size, -1)
+        player_pp = observations["player_pp"].view(batch_size, -1)
 
-        # Battle Embeddings
-        battle_type_input = self.battle_type_embedding(observations["battle_type"].int()).view(batch_size, -1)
-        enemies_left_input = self.enemies_left_embedding(observations["enemies_left"].int()).view(batch_size, -1)
-        player_stats_input = self.player_stats_embedding(observations["player_stats"].view(-1).int()).view(batch_size, -1)
-        enemy_stats_input = self.enemy_stats_embedding(observations["enemy_stats"].view(-1).int()).view(batch_size, -1)
-        turn_info_input = self.turn_info_embedding(observations["turn_info"].view(-1).int()).view(batch_size, -1)
-        type_hint_input = self.type_hint_embedding(observations["type_hint"].int()).view(batch_size, -1)
-        
-        battle_input = torch.cat([
-            pokemon_lineup_input,
-            pokemon_health_input,
-            pokemon_stats_input,
-            battle_type_input,
-            enemies_left_input,
-            player_stats_input,
-            enemy_stats_input,  # TODO: Breakout enemy HP
-            turn_info_input,
-            type_hint_input,
+        moves_input = torch.cat([
+            player_moves_input,
+            player_pp
+
         ], dim=1)
 
-        battle_features = self.battle_fc(battle_input)
+        moves_features = self.move_fc(moves_input)
+        #moves_features = self.move_max_pool(moves_input.unsqueeze(1)).squeeze(1)  # TODO: Try with and without pooling
 
+
+        # Pokemon Class
+        player_pokemon_input = observations["player_pokemon"].view(batch_size, -1)
+        player_levels_input = observations["player_levels"].view(batch_size, -1)
+        player_types_input = observations["player_types"].view(batch_size, -1)
+        player_hp_input = observations["player_hp"].view(batch_size, -1)
+        player_xp_input = observations["player_xp"].view(batch_size, -1)
+        player_stats_input = observations["player_stats"].view(batch_size, -1)
+        player_status_input = observations["player_status"].view(batch_size, -1)
+
+        pokemon_input = torch.cat([
+            player_pokemon_input,
+            player_levels_input,
+            player_types_input,
+            player_hp_input,
+            player_xp_input,
+            player_stats_input,
+            player_status_input,
+        ], dim=1)
+        
+        pokemon_features = self.pokemon_fc(pokemon_input)
+        #pokemon_features = self.player_max_pool(pokemon_input.unsqueeze(1)).squeeze(1)  # TODO: Try with and without pooling
+
+
+        # Player Class
+        player_input = torch.cat([
+            pokemon_features,
+            moves_features,
+        ], dim=1)
+        player_features = self.player_fc(player_input)
+
+        # Player Fighter Class
+        player_head_index = observations["player_head_index"].view(batch_size, -1)
+        player_head_pokemon = observations["player_head_pokemon"].view(batch_size, -1)
+        player_modifiers_input = observations["player_modifiers"].view(batch_size, -1)
+        type_hint_input = observations["type_hint"].view(batch_size, -1)
+
+        player_fighter_input = torch.cat([
+            player_head_index,
+            player_head_pokemon,
+            player_modifiers_input,
+            type_hint_input,
+            player_features,  # TODO: Can we focus on just the head pokemon?, and breakout player to global fc
+        ], dim=1)
+        player_fighter_features = self.player_fighter_fc(player_fighter_input)
+
+
+        # Enemy Battle Class
+        enemy_head_input = observations["enemy_head"].view(batch_size, -1)
+        enemy_level_input = observations["enemy_level"].view(batch_size, -1)
+        enemy_hp_input = observations["enemy_hp"].view(batch_size, -1)
+        enemy_types_input = observations["enemy_types"].view(batch_size, -1)
+        enemy_modifiers_input = observations["enemy_modifiers"].view(batch_size, -1)
+        enemy_status_input = observations["enemy_status"].view(batch_size, -1)
+
+        enemy_battle_input = torch.cat([
+            enemy_head_input,
+            enemy_level_input,
+            enemy_hp_input,
+            enemy_types_input,
+            enemy_modifiers_input,
+            enemy_status_input,
+        ], dim=1)
+        enemy_battle_features = self.enemy_battle_fc(enemy_battle_input)
+
+
+        # Battle Turn Class
+        battle_type_input = observations["battle_type"].view(batch_size, -1)
+        enemies_left_input = observations["enemies_left"].view(batch_size, -1)
+        move_selection_input = observations["move_selection"].view(batch_size, -1)
+
+        battle_turn_input = torch.cat([
+            battle_type_input,
+            enemies_left_input,
+            move_selection_input,
+            player_fighter_features,
+            enemy_battle_features,
+        ], dim=1)
+        battle_turn_features = self.battle_turn_fc(battle_turn_input)
+
+
+        # Final FC layer
         combined_features = torch.cat([
             screen_features,
             coordinates_features,
             action_features, 
             game_state_features,
-            battle_features,
+            battle_turn_features,
+            #battle_features,
         ], dim=1)
 
         # Ensure the input size to fc_combined matches the concatenated features size
@@ -156,7 +275,7 @@ if __name__ == '__main__':
     num_cpu = 124  # Also sets the number of episodes per training iteration
 
     if 0 < num_cpu < 50:
-        env_config['debug'] = True
+        #env_config['debug'] = True
         env_config['headless'] = False
         use_wandb_logging = False
 
@@ -202,10 +321,9 @@ if __name__ == '__main__':
         model.rollout_buffer.n_envs = num_cpu
         model.rollout_buffer.reset()
     else:
-        # policy_kwargs={"features_extractor_class": CustomFeatureExtractor, 
-        #                   "features_extractor_kwargs": {"features_dim": 64}},
-        model = PPO("MultiInputPolicy", env, ent_coef=0.01,
-                    verbose=1, n_steps=2048 // 4, batch_size=512, n_epochs=3, gamma=0.998,  policy_kwargs={"features_extractor_class": CustomFeatureExtractor, "features_extractor_kwargs": {"features_dim": 64}},
+        # policy_kwargs={"features_extractor_class": CustomFeatureExtractor, "features_extractor_kwargs": {"features_dim": 64}},
+        model = PPO("MultiInputPolicy", env, policy_kwargs={"features_extractor_class": CustomFeatureExtractor, "features_extractor_kwargs": {"features_dim": 64}},
+                    verbose=1, n_steps=2048 // 4, batch_size=512, n_epochs=3, gamma=0.998, ent_coef=0.01,
                     seed=GLOBAL_SEED, device="auto", tensorboard_log=sess_path)
 
     print(model.policy)
